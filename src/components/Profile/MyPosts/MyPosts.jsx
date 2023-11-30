b4b8bd9882fecf763b8f1dbd5d04d3c3f484b128
Adina <adinaone04@gmail.com>
2023-11-30 09:39:41 +0600

    Put the posts to outside

--- src/components/Profile/MyPosts/MyPosts.jsx
+++ src/components/Profile/MyPosts/MyPosts.jsx
@@ -2,28 +2,10 @@ import React from 'react';
 import s from './MyPosts.module.css';
 import Post from './Post/Post';
 
-const MyPosts = () =>{
+const MyPosts = (props) =>{
 
-	let posts = [
-		{
-			id:1,
-			message: 'Dattebae',
-			message2 : 'Ramen',
-			likesAmount: 155,
-		},
-		{
-			id:2,
-			message: 'I wanna be Hokage!',
-			likesAmount: 555,
-		},
-		{
-			id:3,
-			message: 'My name is Narutooo!',
-			likesAmount: 5555,
-		},
-	]
 
-	let postsElement = posts.map(p => <Post message={p.message} message2={p.message2} likesAmount={p.likesAmount} />)
+	let postsElement = props.posts.map(p => <Post message={p.message} message2={p.message2} likesAmount={p.likesAmount} />)
 
 	return(
 			<div className={s.postsBlock}>
--- src/components/Profile/Profile.jsx
+++ src/components/Profile/Profile.jsx
@@ -4,10 +4,29 @@ import MyPosts from './MyPosts/MyPosts';
 import ProfileInfo from './ProfileInfo/ProfileInfo';
 
 const Profile = () =>{
+
+	let posts = [
+		{
+			id:1,
+			message: 'Dattebae',
+			message2 : 'Ramen',
+			likesAmount: 155,
+		},
+		{
+			id:2,
+			message: 'I wanna be Hokage!',
+			likesAmount: 555,
+		},
+		{
+			id:3,
+			message: 'My name is Narutooo!',
+			likesAmount: 5555,
+		},
+	]
 	return (
 			<div>
 					<ProfileInfo/>
-					<MyPosts/>
+					<MyPosts posts={posts}/>
 				</div>
 	)
 }
